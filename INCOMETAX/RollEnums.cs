b35afbbb3c33e44e0454f90b7513dafba5fb6c90
kuldeep <kuldeep@ZARVIS>
2018-03-10 19:23:25 +0530

    2

---
+++ INCOMETAX/RollEnums.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INCOMETAX
+{
+    public class RollEnums
+    {
+
+        public enum Rolls
+        {
+            ADMIN = 0 ,
+            OPERATOR=1,
+            OFFICER=2
+            
+        }
+    }
+}
